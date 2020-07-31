3d326681bd906b54422cb0f8e8d1b2370fd639b9
Avin Sharma <sharmaavin@hotmail.com>
2020-07-31 01:11:37 -0400

    Add video player to step details (fixes #3)

--- app/src/main/java/com/example/bakingapp/ui/stepDetails/StepDetailsFragment.java
+++ app/src/main/java/com/example/bakingapp/ui/stepDetails/StepDetailsFragment.java
@@ -1,6 +1,8 @@
 package com.example.bakingapp.ui.stepDetails;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,6 +17,20 @@ import android.widget.TextView;
 
 import com.example.bakingapp.R;
 import com.example.bakingapp.models.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +41,12 @@ public class StepDetailsFragment extends Fragment {
     public static final String TAG = StepDetailsFragment.class.getSimpleName();
     public static final String STEP_KEY = "step-key";
     private TextView mTvDescription;
+    private PlayerView mPlayerView;
+    private SimpleExoPlayer mPlayer;
+
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
 
     public static StepDetailsFragment newInstance(Step step){
         Bundle bundle = new Bundle();
@@ -53,5 +75,76 @@ public class StepDetailsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mTvDescription = view.findViewById(R.id.tv_step_description);
         mTvDescription.setText(mStep.getDescription());
+
+        mPlayerView = view.findViewById(R.id.video_view);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT >= 24) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT < 24 || mPlayer == null)) {
+            initializePlayer();
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        mPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private void initializePlayer() {
+        mPlayer = new SimpleExoPlayer.Builder(getContext()).build();
+        mPlayerView.setPlayer(mPlayer);
+        MediaSource mediaSource = buildMediaSource(Uri.parse(mStep.getVideoURL()));
+        mPlayer.setPlayWhenReady(playWhenReady);
+        mPlayer.seekTo(currentWindow, playbackPosition);
+        mPlayer.prepare(mediaSource, false, false);
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(getContext(), "BakingApp");
+        return new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(uri);
+    }
+
+    private void releasePlayer() {
+        if (mPlayer != null) {
+            playWhenReady = mPlayer.getPlayWhenReady();
+            playbackPosition = mPlayer.getCurrentPosition();
+            currentWindow = mPlayer.getCurrentWindowIndex();
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT < 24) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT >= 24) {
+            releasePlayer();
+        }
     }
 }
